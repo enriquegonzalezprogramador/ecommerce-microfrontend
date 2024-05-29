4d60a874b247ed7d0536865fa861fa9c50133247
Enrique Gonzalez Montes Junior <twd00026@gmail.com>
2024-05-29 16:28:10 -0300

    other commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: true, 
+  },
   plugins: [vue()],
   resolve: {
     alias: {
